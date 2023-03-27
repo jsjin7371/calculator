b632980f58e14f631abd1efe8c9cc460d65d5677
jsj <120244130+jsjin7371@users.noreply.github.com>
2023-03-27 17:11:18 +0900

    Add files via upload

---
+++ 계산기 아이디어2.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//해시 -> 담에는 SHA_256사용해보자
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int (*operation)(int, int);
+
+int add(int a, int b) { return a + b; }
+int subtract(int a, int b) { return a - b; }
+int multiply(int a, int b) { return a * b; }
+int divide(int a, int b) { return a / b; }
+
+int hash_operator(char op) {
+    switch (op) {
+    case '+': return 0;
+    case '-': return 1;
+    case '*': return 2;
+    case '/': return 3;
+    default: return -1;  // invalid operator
+    }
+}
+
+int main() {
+    char op;
+    int operand1, operand2, result;
+    operation operations[4] = { add, subtract, multiply, divide };
+
+    printf("Enter: ");
+    scanf("%d%c%d", &operand1,&op,&operand2);
+
+    int operator_index = hash_operator(op);
+
+    if (operator_index == -1 || operand2 == 0) {
+        printf("error!\n");
+        exit(1);
+    }
+
+    result = operations[operator_index](operand1, operand2);
+    printf("Result: %d\n", result);
+}
---
+++ 계산기 아이디어3.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//파싱
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char c;
+    int num = 0, result = 0;
+    char op = '+';
+
+    while ((c = getchar()) != '\n') {
+        if (isdigit(c)) {
+            //문자열을 숫자로 바꾸기
+            num = num * 10 + (c - '0');
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            switch (op) {
+            case '+':
+                result += num;
+                break;
+            case '-':
+                result -= num;
+                break;
+            case '*':
+                result *= num;
+                break;
+            case '/':
+                result /= num;
+                break;
+            }
+            num = 0;
+            op = c;
+        }
+    }
+
+    switch (op) {
+    case '+':
+        result += num;
+        break;
+    case '-':
+        result -= num;
+        break;
+    case '*':
+        result *= num;
+        break;
+    case '/':
+        result /= num;
+        break;
+    }
+
+    printf("%d\n", result);
+    return 0;
+}
